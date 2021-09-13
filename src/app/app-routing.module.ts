2215058bd95bfc327ed978728838de21ae41228c
Yesid-Q <yesid1995gonzalez@gmail.com>
2021-09-12 23:12:47 -0500

    update module app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+/**
+ * @Import Component page
+ */
+import { AuthComponent } from './pages/auth/auth.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PublicComponent } from './pages/public/public.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: PublicComponent,
+    loadChildren: () => import('./views/public/public.module').then(m => m.PublicModule)
+  },
+  {
+    path: 'auth',
+    component: AuthComponent,
+    loadChildren: () => import('./views/auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    loadChildren: () => import('./views/dashboard/dashboard.module').then(m => m.DashboardModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PublicComponent } from './pages/public/public.component';
+import { AuthComponent } from './pages/auth/auth.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { AsideComponent } from './layout/aside/aside.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicComponent,
+    AuthComponent,
+    DashboardComponent,
+    NavbarComponent,
+    FooterComponent,
+    AsideComponent
   ],
   imports: [
     BrowserModule,
